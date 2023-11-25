1b928bb557d6ee94a6c41c658df9711ec70d2c7a
HerrWal <85037202+HerrWal@users.noreply.github.com>
2023-11-25 18:42:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,47 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-    res.end('Hello World\n');
+
+    // Get the URL and parse it
+    const parsedURL = url.parse(req.url, true);
+    
+    // Get the path
+    const path = parsedURL.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    // Get the query string as an object
+    const queryStringObject = parsedURL.query;   
+
+    // Get the HTTP Method
+    const method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    const headers = req.headers;
+
+    // Get the payload, if any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', data => {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('Hello World\n');
+
+        // Log the request path    
+        console.log('Request is received with this payload: ',buffer);
+    });
 });
 
 // Start the server, and have it listen on port 3000
-server.listen(3000, () => {
-    console.log('The server is listening oin port 3000 now');
+server.listen(3000,() => {
+    console.log('The server is listening on port 3000 now');
 });
